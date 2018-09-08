509e018d37e364004b0a6cb4d9500d30d0981cda
deepakdecode <deepak.kumar@dcodetechnologies.com>
2018-09-08 23:23:43 +0530

    deploy

--- src/pages/Layout/PageFooter.js
+++ src/pages/Layout/PageFooter.js
@@ -7,7 +7,7 @@ class PageFooter extends Component {
                 <a href="#myPage" title="To Top">
                     <span class="glyphicon glyphicon-chevron-up"></span>
                 </a>
-                <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a></p>
+                <p>Bootstrap Theme Made By <a href="/" title="Visit us">www.abcd.com</a></p>
             </footer>
         )
     }
