2b74275171076fb7f41390d2dd2f46df5bf164ad
deepakdecode <deepak.kumar@dcodetechnologies.com>
2018-09-09 10:36:27 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.static(__dirname + '/public'))
 
 // Handles all routes so you do not get a not found error
-app.get('*', function (request, response) {
+app.get('/*', function (request, response) {
     response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
